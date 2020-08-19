896ec47f82f8be999e3aac4c8c2e2c7dd141ffb5
FernandoFreire <nando.neves@gmail.com>
2020-08-19 09:46:29 -0300

    Agora vai

--- novo.sh
+++ novo.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-echo "Qual sua idade?"
-read IDADE
-if [ $IDADE -ge 16 ]
-then
-     echo "Pode votar"
-else 
-    echo "Não pode votar"
-fi
+#Vou ter muito sucesso na programação - Vai dar tudo certo

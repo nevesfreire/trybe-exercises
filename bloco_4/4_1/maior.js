8cbf692d46e43c16fdfaa42448c415a88b87d1ac
Fernando <nando.neves@gmail.com>
2020-08-27 17:51:41 -0300

    4primeiros

---
+++ bloco_4/4_1/maior.js
@@ -0,0 +1,9 @@
+let a =3;
+let b=5;
+if (a<b){
+    return(b);
+} else if (a>b){
+    return(a);
+}else{
+    return("São iguais.");
+}
---
+++ bloco_4/4_1/maior3.js
@@ -0,0 +1,12 @@
+let a = 3;
+let b = 5;
+let c = 7;
+if (a>b $$ a>c){
+    return ("a é maior");
+} else if (b>a && b>c){
+    return (b é maior) 
+} else if (c>a && c>b){
+    return ("c é maior");
+} else {
+    return ("não há maior");
+}

529bdd9289f16139c7e119c106202eb8f329d854
Fernando <nando.neves@gmail.com>
2020-08-27 18:42:05 -0300

    até venda

---
+++ bloco_4/4_1/impar.js
@@ -0,0 +1,6 @@
+let a,b,c;
+if (a%2 !== 0 || b%2 !== 0 || c%2 !== 0){
+    return(true);
+} else{
+    return (false)
+}
---
+++ bloco_4/4_1/venda.js
@@ -0,0 +1,7 @@
+let a //custo
+let b //venda
+if (a<0 ||b<0){
+    return("Erro")
+}else{
+    return("Lucro = " 1000*(b- 1.2*a));
+}

c515dae492968165601a3e1b1249e00eaa0a284f
Fernando <nando.neves@gmail.com>
2020-08-28 18:21:40 -0300

    incluindo bonus

--- bloco_4/4_2/primeiroarray.js
+++ bloco_4/4_2/primeiroarray.js
@@ -69,3 +69,44 @@ for (i=0;i<=ate25.length;i++){
     
     console.log(ate25[i]/2);
 }
+//BONUS 1 -Ordene o array numbers em ordem crescente e imprima seus valores;
+let num = 0;
+
+let crescente = [...numbers];
+for (let j=0; j<crescente.length;j += 1){
+for (let i=0;i<crescente.length;i += 1){
+  if (crescente[i]>crescente[i+1]){
+     num = crescente[i];
+     crescente[i] = crescente[i+1];
+     crescente[i+1] = num; 
+  }
+}
+}
+console.log("Numbers em ordem crescente: "+crescente);
+
+//BONUS 2 Ordene o array numbers em ordem decrescente e imprima seus valores;
+
+let decrescente = [...numbers];
+for (let j=0; j<decrescente.length;j += 1){
+    for (let i=0;i<decrescente.length;i += 1){
+      if (decrescente[i]<decrescente[i+1]){
+         num = decrescente[i];
+         decrescente[i] = decrescente[i+1];
+         decrescente[i+1] = num; 
+      }
+    }
+    }
+    console.log("Numbers em ordem decrescente:" +decrescente);
+
+
+//BONUS 3 Cada valor do novo array deverá ser igual ao valor correspondente no array numbers multiplicado pelo seguinte. 
+let mult=[];
+
+for (let z=0;z<numbers.length;z+=1){
+    if (z!==numbers.length-1){
+    mult.push(numbers[z]*numbers[z+1])
+    } else{
+        mult.push(numbers[numbers.length-1] *2);
+    }
+}
+console.log(mult);

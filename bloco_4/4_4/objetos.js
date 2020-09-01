813c00592f5f3bd39048aba90c423976d380d024
Fernando <nando.neves@gmail.com>
2020-09-01 20:46:02 -0300

    até 7 feito

--- bloco_4/4_4/objetos.js
+++ bloco_4/4_4/objetos.js
@@ -46,11 +46,12 @@ let info = {
         }
       }
       console.log (maior);
+      console.log (array.indexOf(10));
       return(array.indexOf(maior));
   }
-  let teste = [8,5,2,9,6];
+  let teste = [2, 4, 6, 7, 10, 0, -3];
 
-  console.log(maiorValor(teste))
+  console.log("função maior valor :" + maiorValor(teste))
 
   function menorValor (array){
     let menor= array[0];
@@ -64,6 +65,7 @@ let info = {
 }
 console.log(menorValor(teste))
 
+//4
 function nomeLongo (array){
     let umNome =array[0]
     let maior = umNome.length
@@ -76,8 +78,49 @@ function nomeLongo (array){
         indice = i;
     }
     }
-    console.log ("umnome vale "+ umNome + " maior vale "+ maior)
+    //console.log ("umnome vale "+ umNome + " maior vale "+ maior)
     return (array[indice])
 }
 let nomes = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
 console.log (nomeLongo(nomes));
+
+let inteiros = [2, 3, 2, 5, 8, 2, 3];
+
+//5
+function maisFrequente (array){
+    let frequencia=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    let numero=0;
+    for (let k=0; k<array.length;k+=1){
+        numero = frequencia [array[k]]
+        frequencia [array[k]] = numero + 1;
+    }
+    //console.log(frequencia);
+    return (maiorValor (frequencia));
+    }
+console.log("o número mais frequente é o " + maisFrequente(inteiros));
+
+
+//6
+function somatorio(a){
+    let soma =0
+    for(let j=0;j<=a;j+=1){
+        soma = soma + j;
+    }
+    return (soma);
+}
+console.log(somatorio (5));
+
+//7
+function stringEnd (main, ending){
+    let indicador =0;
+    for (let z=0;z<ending.length; z++){
+        if (ending[ending.length-z-1] !== main[main.length-z-1])
+            indicador ++;
+    }
+    if (indicador >0){
+        return(false);
+    }else {
+        return (true);
+    }
+}
+console.log(stringEnd('fernando', 'nando'));

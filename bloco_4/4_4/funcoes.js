2cf5311c2c3811cf5d9d3f184428fe0bec1304aa
Fernando <nando.neves@gmail.com>
2020-09-01 15:49:12 -0300

    Primeiro feito

---
+++ bloco_4/4_4/funcoes.js
@@ -0,0 +1,17 @@
+//1
+function sum (a,b){
+    return (a+b);
+}
+function sub (a,b){
+    return (a-b);
+}
+function div (a,b){
+    return (a/b);
+}
+function mult (a,b){
+    return (a*b);
+}
+function mod (a,b){
+    return (a%b);
+}
+//2

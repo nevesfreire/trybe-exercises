ba045e81d7525e04b67c87f7f37a048df282815a
Fernando <nando.neves@gmail.com>
2020-09-01 16:14:02 -0300

    até 8

--- bloco_4/4_4/funcoes.js
+++ bloco_4/4_4/funcoes.js
@@ -49,4 +49,52 @@ function positiveNegative(a){
 }
 console.log (positiveNegative(-10));
 console.log (greater3(5,7,9));
-//5
+//5 
+function angulos (a, b, c){
+    if (a+b+c=180){
+        return (true);
+    } else {
+        return (false);
+    }
+}
+//6
+function movement (a)
+switch (a){
+    case "peon"
+    return ("Em frente");
+    break
+    case "knight"
+    return ("Em L);
+    break
+    case "Tower"
+    return ("Linha reta");
+    break
+    case "bishop"
+    return ("diagonals");
+    break
+    case "queen"
+    return ("Todos os lados");
+    break
+    case "king"
+    return ("Todos os lados, apenas 1 casa");
+    break
+}
+//7
+function grade (nota){
+    if (nota>=90){
+        return ("A");
+    } else if (nota>=80){
+        return ("B");
+    }else if (nota>=70){
+        return ("C");
+    } else if (nota>=60){
+        return("D")
+    } else if (nota>=50){
+        return("E")
+    } else if (nota <50){
+        return ("F")
+    } else {
+        return "Erro"
+    }
+}
+//8

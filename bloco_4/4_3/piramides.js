72ff79468c3bd519bca84681fcd760de0f05ed60
Fernando <nando.neves@gmail.com>
2020-09-01 15:44:05 -0300

    finalizado

--- bloco_4/4_3/piramides.js
+++ bloco_4/4_3/piramides.js
@@ -1,6 +1,6 @@
 //1 - Fazer quadrado de asteriscos:
 
-let base = 7;
+let base = 5;
 let linha=["*"];
 for (let i=1; i<base;i+=1){
     linha = linha + "*";
@@ -80,3 +80,51 @@ if(base%2!=0){
     }else{
     console.log('Não funciona com base par');
 }
+
+// 6 - é primo ou não. 
+let numero =567;
+let divisores =0;
+for (i=0;i<=numero;i+=1){
+    if (numero%i ==0){
+        divisores++;
+    }
+}
+if (divisores ==2){
+    console.log (numero + " é primo.");
+}else{
+    console.log(numero + " não é primo.")
+}
+console.log(divisores);
+
+//7 - fazer array com número s primos
+console.log("lista de primos")
+let primos =[];
+let contador=0;
+for (j=0;j<10;j++){ //define o intervalo (0-j) onde você está procurando números sprimos. 
+    contador=0;
+    for (z=0;z<=j;z+=1){
+        if (j%z==0){
+            contador +=1;
+        }
+    }
+    if (contador ==2){
+        primos.push(j);
+    }
+
+}
+
+console.log("Os números primos nesse intervalo são: " + primos);
+
+//Posição da rainha
+
+let colunaRainha = 4;
+let linhaRainha = 4;
+let linhaPeca = 7;
+let colunaPeca =1;
+let ataqueBemSucedido = false;
+
+if (colunaRainha === colunaPeca || linhaRainha === linhaPeca || colunaRainha-colunaPeca === linhaRainha-linhaPeca || colunaRainha-colunaPeca === -(linhaRainha-linhaPeca)){
+    ataqueBemSucedido = true;
+}
+
+console.log (ataqueBemSucedido);

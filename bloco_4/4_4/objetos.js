a8c3d90f7a61abf23725503488280150fec9aa02
Fernando <nando.neves@gmail.com>
2020-09-01 18:58:31 -0300

    até nome longo

--- bloco_4/4_4/objetos.js
+++ bloco_4/4_4/objetos.js
@@ -24,4 +24,60 @@ let info = {
 
   //funções
 
+  function palindromo (a){
+      let verificador = 0;
+      for (let i=0;i<a.length;i+=1){
+          if (a[i] !=a[a.length-1-i]){
+              verificador+=1;
+          }
+      }
+      if (verificador >0){
+          return (false);  
+      } else{
+          return (true);
+      }
+  }
   
+  function maiorValor (array){
+      let maior= array[0];
+      for (let i in array){
+        if (array[i]>maior){
+            maior = array[i];
+        }
+      }
+      console.log (maior);
+      return(array.indexOf(maior));
+  }
+  let teste = [8,5,2,9,6];
+
+  console.log(maiorValor(teste))
+
+  function menorValor (array){
+    let menor= array[0];
+    for (let i in array){
+      if (array[i]< menor){
+          menor = array[i];
+      }
+    }
+    console.log (menor);
+    return(array.indexOf(menor));
+}
+console.log(menorValor(teste))
+
+function nomeLongo (array){
+    let umNome =array[0]
+    let maior = umNome.length
+    let indice
+
+    for (let i in array){
+        umNome = array[i];
+    if (umNome.length > maior){
+        maior = umNome.length;
+        indice = i;
+    }
+    }
+    console.log ("umnome vale "+ umNome + " maior vale "+ maior)
+    return (array[indice])
+}
+let nomes = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+console.log (nomeLongo(nomes));

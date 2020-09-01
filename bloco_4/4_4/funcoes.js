3e6844a7f04ae0301c654e8baa2a0e263f994254
Fernando <nando.neves@gmail.com>
2020-09-01 16:03:42 -0300

    até 4

--- bloco_4/4_4/funcoes.js
+++ bloco_4/4_4/funcoes.js
@@ -15,3 +15,38 @@ function mod (a,b){
     return (a%b);
 }
 //2
+function greater2 (a,b){
+    if (a>b){
+    return ("o maior é " + a);
+    } else if (b>a){
+        return ("o maior é " + b);
+    }else {
+        return ("são iguais");
+    }
+
+}
+//3 
+function greater3 (a,b,c){
+    if(a>b && a>c){
+        return ( a +" é o maior.");
+    } else if (b>c && b>a){
+    return (b + " é o maior.");
+} else if (c>a &&c>b){
+    return (c + " é o maior");
+} else{
+    return ("não há maior.");
+}
+}
+//4
+function positiveNegative(a){
+    if (a>0){
+        return ("positive");
+    }else if (a<0){
+        return ("negative");
+    } else{
+        return ("zero");
+    }
+}
+console.log (positiveNegative(-10));
+console.log (greater3(5,7,9));
+//5

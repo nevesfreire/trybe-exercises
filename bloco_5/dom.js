34828c987322796f614c58277ed30d0440f6de60
Fernando <nando.neves@gmail.com>
2020-09-09 12:37:15 -0300

    fim

---
+++ bloco_5/dom.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8"></meta>
+    <title> HellWorld</title>
+    <style>
+      .principal{
+        color: red;
+        }
+    </style>
+  </head>
+  <body>
+    <h2 id="page-title" class="principal">Título</h2>
+    <p id="paragraph">Dê duma cor para este parágrafo!</p>
+    <h4 id="subtitle" class= "principal">Subtítulo</h4>
+    <p id="second-paragraph">Segundo parágrafo!</p>
+
+    <script src="script.js">   </script>
+  </body>
+</html>
---
+++ bloco_5/script.js
@@ -0,0 +1,5 @@
+var paragraph = document.getElementById("paragraph");
+      paragraph.style.color = "red";
+
+
+

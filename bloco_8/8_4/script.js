f6508ebaaa9e0460ab3072466d5f2afb7b782639
Fernando <nando.neves@gmail.com>
2020-10-08 19:20:30 -0300

    metade

---
+++ bloco_8/8_4/script.js
@@ -0,0 +1,127 @@
+// Dado o código abaixo, complete-o de forma que seja impressa a área dos 3 retângulos. 
+// O código deve retornar em sequência 2, 15 e 54.
+
+const assert = require('assert')
+
+const rectangleArea = (width, height) => width * height;
+
+const rectangle1 = [1, 2]
+const rectangle2 = [3, 5]
+const rectangle3 = [6, 9]
+const rectangles = [rectangle1, rectangle2, rectangle3]
+
+for(rectangle of rectangles) {
+    assert.strictEqual(rectangleArea(...rectangle), rectangle[0]*rectangle[1]) // altere a chamada da funcao rectangleArea
+}
+
+// Crie uma função sum que dado um número variável de elementos retorna a soma desses 
+// elementos. 
+
+// escreva sum abaixo
+const sum = (...numbers) => {let total = 0; for (i in numbers) {total+=numbers[i]}; return total;}
+
+assert.strictEqual(sum(), 0)
+assert.strictEqual(sum(1), 1)
+assert.strictEqual(sum(1, 2), 3)
+assert.strictEqual(sum(1, 2, 3), 6)
+assert.strictEqual(sum(1, 2, 3, 4), 10)
+
+// Escreva a função personLikes, que dado um objeto de parâmetro que representa uma pessoa, 
+// retorna todos os gostos daquela pessoa, conforme mostrado abaixo:
+
+const alex = {
+    name: "Alex",
+    age: 26,
+    likes: ["fly fishing"],
+    nationality: "Australian"
+}
+
+const gunnar = {
+    name: "Gunnar",
+    age: 30,
+    likes: ["hiking", "scuba diving", "taking pictures"],
+    nationality: "Icelandic"
+}
+
+// complete a assinatura da função abaixo
+const personLikes = ({name,age,likes, nationality}) => `${name} is ${age} years old and likes ${likes.join(", ")}.`
+assert.strictEqual(personLikes(alex), "Alex is 26 years old and likes fly fishing.")
+assert.strictEqual(personLikes(gunnar), "Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.")
+
+// Escreva uma função filterPeople que, dada uma lista de pessoas, 
+// retorna todas as pessoas australianas que nasceram no século 20:
+
+const people = [
+    {
+        name: "Nicole",
+        bornIn: 1992,
+        nationality: "Australian"
+    },
+    {
+        name: "Harry",
+        bornIn: 2008,
+        nationality: "Australian"
+    },
+    {
+        name: "Toby",
+        bornIn: 1901,
+        nationality: "Australian"
+    },
+    {
+        name: "Frida",
+        bornIn: 1960,
+        nationality: "Dannish"
+    },
+    {
+        name: "Fernando",
+        bornIn: 2001,
+        nationality: "Brazilian"
+    }
+]
+
+// escreva filterPeople abaixo
+
+const filteredPeople = people.filter((person)=> {
+  const {name, bornIn, nationality} = person;
+   return (nationality === "Australian" && bornIn < 2000);
+ }); 
+
+assert.deepStrictEqual(filteredPeople[0], { name: "Nicole", bornIn: 1992, nationality: "Australian" })
+assert.deepStrictEqual(filteredPeople[1], { name: "Toby", bornIn: 1901, nationality: "Australian" })
+
+// Escreva a função swap, que dado um array de 3 elementos, 
+// retorna um novo array com o primeiro e terceiro elementos trocados. 
+// Detalhe: você precisa fazer essa função gastando 1 linha só:
+
+const myList = [1, 2, 3]
+
+// escreva swap abaixo
+
+const swappedList = swap(myList) 
+// const assert = require('assert')
+function swap (myList) {const [a,b,c] = myList; return [c,b,a];}
+assert.strictEqual(swappedList[0], 3)
+assert.strictEqual(swappedList[1], 2)
+assert.strictEqual(swappedList[2], 1)
+
+// Suponha que você esteja lidando com carros e, da forma como o problema lhe foi 
+// entregue, cada carro é modelado como um array. Porém, essa modelagem está baixo nível. 
+// Cria uma função toObject que, dada uma lista, retorna um objeto representando o carro:
+
+
+const palio = ["Palio", "Fiat", 2019]
+const shelbyCobra = ["Shelby Cobra", "Ford", 1963]
+const chiron = ["Chiron", "Bugatti", 2016]
+
+// escreva toObject abaixo
+function toObject(array) {
+  [name, brand,year] =array;
+  return {
+    name,
+    brand,
+    year,
+  }
+}
+assert.deepStrictEqual(toObject(palio), { name: "Palio", brand: "Fiat", year: 2019 })
+assert.deepStrictEqual(toObject(shelbyCobra), { name: "Shelby Cobra", brand: "Ford", year: 1963 })
+assert.deepStrictEqual(toObject(chiron), { name: "Chiron", brand: "Bugatti", year: 2016 })

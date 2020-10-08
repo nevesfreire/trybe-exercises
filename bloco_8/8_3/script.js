e27adaf5b4d6b685687a6ff64cf62197640502f8
Fernando <nando.neves@gmail.com>
2020-10-07 23:11:03 -0300

    todos 8_3 feitos

--- bloco_8/8_3/script.js
+++ bloco_8/8_3/script.js
@@ -263,3 +263,34 @@ function containsA() {
 // console.log(containsA())
 assert.deepStrictEqual(containsA(), 20);
 
+// Agora vamos criar um novo array de objetos a partir das informações abaixo, onde cada 
+// objeto terá o formato { name: nome do aluno, average: media das notas }. Para isso vamos
+//  assumir que a posição 0 de notas refere-se ao aluno na posição 0 de alunos, aqui além 
+//  de reduce será necessário utilizar também a função map. Dica: Você pode acessar o index 
+//  do array dentro de map, e você pode ver o objeto esperado na constante expected.
+
+    
+// const assert = require('assert');
+
+const alunos = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const notas = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+
+const media = (number) => number.reduce((total,one)=>{total =total +one; return total;})
+
+
+function studentAverage() {
+  const result = alunos.map((item,i)=> {return{name: item, 
+    average: media(notas[i])/5 }})
+  
+  return result;
+  // escreva seu código aqui
+}
+// console.log(studentAverage())
+const expected = [
+  { name: 'Pedro Henrique', average: 7.8 },
+  { name: 'Miguel', average: 9.2 },
+  { name: 'Maria Clara', average: 8.8 },
+];
+
+assert.deepStrictEqual(studentAverage(), expected);
